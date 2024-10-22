d6605679874671bfb942683249f47ac22d2c2bc6
Muhannad <muhanned9980@gmail.com>
2024-10-22 10:11:19 +0300

    adding the application Schema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,26 @@
 const mongoose = require('mongoose');
+// models/user.js
+
+const applicationSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+  },
+});
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -10,6 +32,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema],
 });
 
 module.exports = mongoose.model('User', userSchema);
