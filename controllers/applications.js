b743256cc331adf74428fba4157f100b03156ed5
Muhannad <muhanned9980@gmail.com>
2024-10-22 16:10:34 +0300

    adding the input of the aplication from the user

--- controllers/applications.js
+++ controllers/applications.js
@@ -6,22 +6,29 @@ const router = express.Router();
 const User = require('../models/user.js');
 
 // we will build out our router logic here
-router.get('/', async (req ,res) =>{
-    try {
-        res.render('applications/index.ejs');
-      } catch (error) {
-        console.log(error)
-        res.redirect('/')
-      }
+router.get('/', async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Render index.ejs, passing in all of the current user's 
+    // applications as data in the context object. 
+    res.render('applications/index.ejs', {
+      applications: currentUser.applications,
+    });
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error)
+    res.redirect('/')
+  }
 });
 
-// controllers/applications.js
+
 
 router.get('/new', async (req, res) => {
   res.render('applications/new.ejs');
 });
 
-// controllers/applications.js`
+
 
 router.post('/', async (req, res) => {
   try {
