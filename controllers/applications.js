d2111d0e70e3987b3dc308f68b8ab5d146a1d81b
Muhannad <muhanned9980@gmail.com>
2024-10-22 16:02:17 +0300

    adding new application form

--- controllers/applications.js
+++ controllers/applications.js
@@ -21,4 +21,27 @@ router.get('/new', async (req, res) => {
   res.render('applications/new.ejs');
 });
 
+// controllers/applications.js`
+
+router.post('/', async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+
+    // Modify the date part of the form
+    req.body.date = new Date(req.body.date)
+
+    // Push req.body (the new form data object) to the
+    // applications array of the current user
+    currentUser.applications.push(req.body);
+    // Save changes to the user
+    await currentUser.save();
+    // Redirect back to the applications index view
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/')
+  }
+});
 module.exports = router;
